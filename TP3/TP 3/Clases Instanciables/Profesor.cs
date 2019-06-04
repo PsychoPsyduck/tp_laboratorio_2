557e15dbb72256b1ba254df157b3b30a38f81f26
psychopsyduck <nialsande@gmail.com>
2019-06-03 21:29:24 -0300

    terminado tp 3

---
+++ TP3/TP 3/Clases Instanciables/Profesor.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+
+namespace Clases_Instanciables
+{
+    public class Profesor : Universitario
+    {
+        private Queue<Universidad.EClases> clasesDelDia;
+        private static Random random;
+
+        /// <summary>
+        /// Constructor estatico de profesor, inicializa el random
+        /// </summary>
+        static Profesor()
+        {
+            random = new Random();
+        }
+
+        /// <summary>
+        /// constructor vacio
+        /// </summary>
+        private Profesor()
+        {
+        }
+
+        /// <summary>
+        /// Constructor de Profesor que declara id, nombre, apellido, dni, nacionalidad y le declara las clases del dia
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Profesor(int id, string nombre, string apellido, string dni, 
+                ENacionalidad nacionalidad) : base(id, nombre, apellido, 
+                dni, nacionalidad)
+        {
+            clasesDelDia = new Queue<Universidad.EClases>();
+            _randomClases();
+        }
+
+        /// <summary>
+        /// muestar que clases toma
+        /// </summary>
+        /// <returns></returns>
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder retorno = new StringBuilder();
+            retorno.AppendLine("CLASES DEL DIA:");
+            foreach (Universidad.EClases c in clasesDelDia)
+            {
+                retorno.Append(" " + c.ToString());
+            }
+            retorno.Append("\n");
+            return retorno.ToString();
+        }
+
+        /// <summary>
+        /// muestra los datos del progfesor
+        /// </summary>
+        /// <returns></returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder retorno = new StringBuilder();
+            //retorno.AppendLine("\nProfesor:");
+            retorno.AppendLine(base.MostrarDatos());
+            retorno.AppendLine(ParticiparEnClase());
+            return retorno.ToString();
+        }
+
+        /// <summary>
+        /// imprime todos los datos del profesor juntos con la clases que toma
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return MostrarDatos();
+        }
+
+        /// <summary>
+        /// randomiza las clases a tomar por el profesor.
+        /// </summary>
+        private void _randomClases()
+        {
+            clasesDelDia.Enqueue((Universidad.EClases)random.Next(5));
+            System.Threading.Thread.Sleep(1000);
+            clasesDelDia.Enqueue((Universidad.EClases)random.Next(4));
+        }
+
+        ///Operadores Logicos
+        public static bool operator ==(Profesor i, Universidad.EClases clase)
+        {
+            foreach (Universidad.EClases c in i.clasesDelDia)
+            {
+                if (clase == c)
+                    return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Profesor i, Universidad.EClases clase)
+        {
+            return !(i == clase);
+        }
+    }
+}
---
+++ TP3/TP 3/Clases abstractas/Persona.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+    {
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+
+        /// <summary>
+        /// constructor vacio de Persona
+        /// </summary>
+        public Persona()
+        {
+        }
+
+        /// <summary>
+        /// Contstructor de persona que declara nombre, apellido y nacionalidad
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad) : this()
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nacionalidad = nacionalidad;
+        }
+
+        /// <summary>
+        /// constructor de persona que ademas declara el dni en int
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, int dni, 
+                ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            DNI = dni;
+        }
+
+        /// <summary>
+        /// constructor de persona que declara el dni en string
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, string dni, 
+                ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            StringToDni = dni;
+        }
+
+        /// <summary>
+        /// valida que el dni sea de un numero valido
+        /// </summary>
+        /// <param name="nacionalidad"></param>
+        /// <param name="dato"></param>
+        /// <returns></returns>
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            if(nacionalidad == ENacionalidad.Argentino && (dato > 0 && dato < 90000000))
+            {
+                return dato;
+            }
+            else if(nacionalidad == ENacionalidad.Extranjero && (dato > 89999999 && dato <= 99999999))
+            {
+                return dato;
+            }
+            else
+            {
+                throw new NacionalidadInvalidaException("Nacionalidad invalida");
+            }
+        }
+
+        /// <summary>
+        /// valida que el dni no tenga caracteres erroneos o sea mas largo 
+        /// </summary>
+        /// <param name="nacionalidad"></param>
+        /// <param name="dato"></param>
+        /// <returns></returns>
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int dni;
+
+            if(!(dato.Length > 0 && dato.Length < 9))
+            {
+                throw new DniInvalidoException("Dni invalido");
+            }
+
+            foreach (char c in dato)
+            {
+                if (!(char.IsNumber(c)))
+                {
+                    throw new DniInvalidoException("Dni invalido");
+                }
+            }
+
+            if(!(int.TryParse(dato, out dni)))
+            {
+                throw new DniInvalidoException("Dni invalido");
+            }
+            else
+            {
+                return ValidarDni(nacionalidad, dni);
+            }
+        }
+
+        /// <summary>
+        /// valida el nombre o apellido, que no tenga caracteres numericos ni puntuaciones
+        /// </summary>
+        /// <param name="dato"></param>
+        /// <returns></returns>
+        private string ValidarNombreApellido(string dato)
+        {
+            foreach (char c in dato)
+            {
+
+                if (!(char.IsLetter(c) && !(char.IsPunctuation(c)) && !(char.IsWhiteSpace(c)) && !(char.IsNumber(c))))
+                {
+                    return "";
+                }
+            }
+            return dato;
+        }
+
+        /// <summary>
+        /// Imprime el nombre y apellido, dni y nacionalidad
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder retorno = new StringBuilder();
+            retorno.AppendFormat("Nombre: {0}, {1}\n", Apellido, Nombre);
+            retorno.AppendFormat("Dni: {0}\n", DNI);
+            retorno.AppendFormat("Nacionalidad: {0}\n", Nacionalidad.ToString());
+            return retorno.ToString();
+        }
+
+        /// Propiedades
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                apellido = ValidarNombreApellido(value);
+            }
+        }
+        public int DNI
+        {
+            get
+            {
+                return dni;
+            }
+            set
+            {
+                dni = ValidarDni(Nacionalidad, value);
+            }
+        }
+        public ENacionalidad Nacionalidad
+        {
+            get
+            {
+                return nacionalidad;
+            }
+            set
+            {
+                nacionalidad = value;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = ValidarNombreApellido(value);
+            }
+        }
+        public string StringToDni
+        {
+            set
+            {
+                DNI = ValidarDni(Nacionalidad, value);
+            }
+        }
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+    }
+}
